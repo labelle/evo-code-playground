76e699c0a571e5a138fcf3a2ab1965f1f0f969f5
Dan Merfeld <dan@theorythree.com>
2016-12-06 16:25:26 -0600

    extracts js into independent file for dev purposes

---
+++ js/evo-playground.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+  $.get("./tbl/html/index.html", function(response) {
+      html = response;
+      $('#box1 pre').text(response);
+      $('#box4').html('<div id="stage">'+html+'</div>');
+
+      $.get("./tbl/css/index.css", function(response) {
+        css = response;
+        $('#box2 pre').text(response);
+        $('#box4 div').append('<style>'+response+'</style>');
+
+        $.get("./tbl/js/index.txt", function(response) {
+          js = response;
+          $.getScript("./tbl/js/index.txt");
+          $('#box3 pre').text(response);
+        });
+
+      });
+
+  });
+
+  $(document).on('keyup','#box1 pre',function(){
+    console.log("edit me edit");
+
+    // delay
+    $(this).parseMe();
+
+  });
+
+$.fn.parseMe = function () {
+
+  var html = $('#box1 pre').html();
+  var css = $('#box2 pre').html();
+  var js = $('#box3 pre').val();
+
+  $('#box4').html('<div id="stage">'+decodeEntities(html)+' </div>');
+  $('#box4 div').append('<style>'+decodeEntities(css)+'</style>');
+  //$.getScript(js);
+
+};
+
+function decodeEntities(encodedString) {
+    var textArea = document.createElement('textarea');
+    textArea.innerHTML = encodedString;
+    return textArea.value;
+}
+
+$(this).parseMe();
+
+});
